0835718c9cf2d29b7ce4f44b01b6d3242bdec068
Youssef Salem <yssalem12@gmail.com>
2024-02-18 08:29:35 -0700

    Added express and ejs

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const fs = require('fs');
+const path = require('path');
+
+app.use( express.static( "public" ) );
+
+app.get('/', (req, res) => {
+  res.render((__dirname + '/index.ejs'), { __dirname : path.join(__dirname, "..") })
+})
+
+app.get('/index.html', (req, res) => {
+    res.render((__dirname + '/index.ejs'))
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile((path.join(__dirname, "..") + '/public/src/login.html'))
+})
+
+app.get('/signup.html', (req, res) => {
+    res.sendFile((path.join(__dirname, "..") + '/public/src/signup.html'))
+})
+
+app.get('/contracts.html', (req, res) => {
+    res.sendFile((path.join(__dirname, '..') + '/public/src/contracts.html'))
+})
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
