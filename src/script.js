75c04b6a31778e4428ad602dcc8c3f4945e3f01b
Dubi-1 <60275618+MohammadHashmi@users.noreply.github.com>
2024-02-18 08:35:38 -0700

    bazaar page texting WIP

---
+++ src/script.js
@@ -0,0 +1,38 @@
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
+const user = auth.currentUser;
+if (user !== null) {
+  // The user object has basic properties such as display name, email, etc.
+  const email = user.email;
+  const uid = user.uid;
+
+  Talk.ready.then(function () {
+    const me = new Talk.User({
+      id: uid,
+      email: email,
+    });
+    window.talkSession = new Talk.Session({
+      appId: 'tLLHh84Q',
+      me: me,
+    });
+    var other = new Talk.User({
+      id: '654321',
+      name: 'Sebastian',
+      email: 'Sebastian@example.com',
+      photoUrl: 'https://talkjs.com/images/avatar-5.jpg',
+      welcomeMessage: 'Hey, how can I help?',
+    });
+  
+    var conversation = talkSession.getOrCreateConversation(
+      Talk.oneOnOneId(me, other)
+    );
+    conversation.setParticipant(me);
+    conversation.setParticipant(other);
+  
+    var inbox = talkSession.createInbox();
+    inbox.select(conversation);
+    inbox.mount(document.getElementById('talkjs-container'));
+  });
+}
+
